5a6cf22ae60f60cb03bd2cdb5b8d70fbda4093f5
Marcel Slootweg <mslootweg@viktor.ai>
2022-04-15 16:44:43 +0200

    changes made by Marcel, including memoization

--- app/residential_block/controller.py
+++ app/residential_block/controller.py
@@ -1,3 +1,4 @@
+
 from viktor.core import ViktorController
 from viktor.views import Summary, DataGroup, GeometryAndDataView, GeometryAndDataResult, PDFView, PDFResult
 from viktor.views import MapView, MapResult
@@ -11,7 +12,8 @@ from viktor.external.word import WordFileTag, render_word_file
 from .house_on_map import house_on_map
 
 from .parametrization import ResidentialBlockParametrization
-from .run_dynamo import run_dynamo
+from .dynamo_utils import run_dynamo, create_dynamo_file, from_decode_to_file, convert_dynamo_file_to_data_items, \
+    get_dynamo_output_results, decode_file
 
 
 class ResidentialBlockController(ViktorController):
@@ -21,11 +23,23 @@ class ResidentialBlockController(ViktorController):
     children = []
     show_children_as = None
 
+    @staticmethod
+    def run_dynamo_from_params(params):
+        """This method runs the dynamo model by using the params as input."""
+        # Step 1: create a Dynamo input file
+        dynamo_input_file = create_dynamo_file(params)
+        # Step 2: Run the dynamo model. As the dynamo run is memoized, the input file needs
+        #  to be json serializable. Therefore the conversion first needs to take place.
+        serialized_input_file = decode_file(dynamo_input_file.generate())
+        decoded_output_files = run_dynamo(serialized_input_file)
+        # The output files are returned as decoded strings. These first then need to be converted
+        #  back to their preferred formats.
+        glb_file = from_decode_to_file(decoded_output_files['glb_file'])
+        output_file = from_decode_to_file(decoded_output_files['output_file'])
+        return dynamo_input_file, glb_file, output_file
+
     @GeometryAndDataView("Building 3D", duration_guess=5)
     def geometry_and_data_view(self, params, **kwargs):
-
-        # Run the dynamo module and retrieve output geometry and data
-        glb_file, dataview_output_items, dynamo_output_report = run_dynamo(params)
 
         # Some exceptions are raised to direct the user of the application in a certain direction.
         if params.step_2.number_of_houses > params.step_1.number_of_houses:
@@ -39,6 +53,10 @@ class ResidentialBlockController(ViktorController):
         if params.step_2.depth > params.step_1.depth:
             raise UserException(
                 "The depth is larger than provided in step 1! Please go back to step 1 and increase the depth.")
+
+        # Run the dynamo module and retrieve output geometry and data
+        dynamo_input_file, glb_file, output_file = self.run_dynamo_from_params(params)
+        dataview_output_items = convert_dynamo_file_to_data_items(dynamo_input_file, output_file)
 
         return GeometryAndDataResult(geometry=glb_file, data=DataGroup(*dataview_output_items))
 
@@ -62,15 +80,18 @@ class ResidentialBlockController(ViktorController):
         return MapResult(features)
 
     def download_file(self, params, **kwargs):
+        """This method is triggered when clicking the 'Download report (docx)' button."""
         word_file = self.render_word_report(params)
 
         return DownloadResult(word_file, 'my_word_file.docx')
 
-    @staticmethod
-    def render_word_report(params):
+    def render_word_report(self, params):
         """This method creates a word file report based on the user input parameters and the numerical output from
         the dynamo model"""
-        glb_file, dataview_output_items, dynamo_output_report = run_dynamo(params)
+
+        # Run the dynamo module and retrieve output geometry and data
+        dynamo_input_file, glb_file, output_file = self.run_dynamo_from_params(params)
+        dynamo_output_results = get_dynamo_output_results(dynamo_input_file, output_file)
 
         template_path = Path(__file__).parent.parent / "lib" / "files" / "sample_document.docx"
         components = [WordFileTag('n_houses', params.step_2.number_of_houses),
@@ -78,28 +99,11 @@ class ResidentialBlockController(ViktorController):
                       WordFileTag('n_floors', params.step_2.number_of_floors),
                       WordFileTag('height_floor', params.step_2.height_floor),
                       WordFileTag('height_roof', params.step_2.height_roof),
-                      WordFileTag('floor_area', dynamo_output_report['result_floor_area'][0]),
-                      WordFileTag('total_cost', dynamo_output_report['result_total_cost'][0]),
-                      WordFileTag('MKI', dynamo_output_report['result_MKI'][0]),
-                      WordFileTag('CO2', dynamo_output_report['result_floor_CO2'][0])]
+                      WordFileTag('floor_area', dynamo_output_results['result_floor_area']['value']),
+                      WordFileTag('total_cost', dynamo_output_results['result_total_cost']['value']),
+                      WordFileTag('MKI', dynamo_output_results['result_MKI']['value']),
+                      WordFileTag('CO2', dynamo_output_results['result_floor_CO2']['value'])]
 
         with open(template_path, 'rb') as template:
             word_file = render_word_file(template, components)
         return word_file
-
-
-class InvalidDynamoFile(KeyError):
-    def __init__(self):
-        super().__init__("Invalid Dynamo Script format. File should contain 'Inputs', which should contain 'Name' and "
-                         "'Id', and 'Nodes', which should contain 'Id' and 'InputValue'")
-
-
-class InvalidMapping(KeyError):
-    def __init__(self, param_mapping: str):
-        super().__init__(f"Invalid mapping of parametrization to Dynamo inputs. No input with name '{param_mapping}'"
-                         f" found ")
-
-
-class UnknownDynamoInputType(ValueError):
-    def __init__(self, dynamo_type: str):
-        super().__init__(f"Input of type '{dynamo_type}' unknown. Add to code or adjust input.")
